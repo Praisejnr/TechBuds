5a302c938fb92cf4ce8c389242dc5b61fc40d25e
Praisejnr <praiseomoniyi65@gmail.com>
2025-08-02 01:24:12 -0700

    update css

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const navBar = document.getElementById("nav-bar");
 const hamburger = document.querySelector(".hamburger");
+const frontImg = document.getElementById("front-img");
 
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
+
+const images = ["/images/img1.png", "/images/img2.png", "/images/img3.png"];
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  frontImg.src = images[index];
+}, 5000);
