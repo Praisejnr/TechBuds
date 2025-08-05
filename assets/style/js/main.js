26f9b28de554d8e94888c4acb92e1c883402cecc
Praisejnr <praiseomoniyi65@gmail.com>
2025-08-04 21:58:33 -0700

    commit

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector(".hamburger");
 const frontImg = document.getElementById("front-img");
 const front = document.querySelector(".front");
 const modeSwitch = document.querySelector(".switch");
+const switchButton = document.getElementById("modeSwitch");
 
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
@@ -18,20 +19,25 @@ let index = 0;
 setInterval(() => {
   index = (index + 1) % images.length;
   frontImg.src = images[index];
-}, 5000);
+}, 3000);
 
 let isDarkMode = false;
 modeSwitch.addEventListener("click", () => {
   if (!isDarkMode) {
+    modeSwitch.innerHTML = `Light mode <i class="fa-solid fa-toggle-off"></i
+          >`;
     document.body.style.backgroundColor = "#111";
-    document.querySelectorAll("h1, p").forEach((tags) => {
+    document.querySelectorAll("p").forEach((tags) => {
       tags.style.color = "#111";
     });
   } else {
+    modeSwitch.innerHTML = `Dark mode <i class="fa-solid fa-toggle-on"></i
+          >`;
     document.body.style.backgroundColor = "#fff";
-    document.querySelectorAll("h1, p").forEach((tags) => {
+    document.querySelectorAll("p").forEach((tags) => {
       tags.style.color = "#fff";
     });
   }
   isDarkMode = !isDarkMode;
+  localStorage.setItem("darkMode", isDarkMode);
 });
